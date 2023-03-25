f6162fae449c57b6f0f30bf7a2969e246b2e8b3d
Satyanarayan pandey <satyampandey91298@gmail.com>
2023-03-26 02:02:29 +0530

    first commit

---
+++ src/main/java/com/payment/service/SvcPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.payment.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvcPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvcPaymentServiceApplication.class, args);
+	}
+
+}
